b4d22de90a58f04aef74b7bbf75c5ce6727c40d2
JavokhirAbdurazzokov <168398916+JavokhirAbdurazzokov@users.noreply.github.com>
2024-10-24 20:54:09 +0500

    Add files via upload

---
+++ 1-dars.py
@@ -0,0 +1,11 @@
+ism = "Javokhir"
+familiya = "Abdurazzokov"
+otchestva = "Hakim ogli"
+sana = "25may 2010Yil"
+adres = "Chilonzor S kvartal"
+yosh = 14
+
+full_name = ism+" "+familiya+" ", yosh+otchestva+sana+adres
+
+print(full_name)
+
